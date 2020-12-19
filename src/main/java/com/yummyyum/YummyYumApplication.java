3397d6b04da5cea19026f1bae75200fab7e285f4
PajkoStefan <anastasovski.stefan96@gmail.com>
2020-12-19 17:39:31 +0100

    Initial commit

---
+++ src/main/java/com/yummyyum/YummyYumApplication.java
@@ -0,0 +1,13 @@
+package com.yummyyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyYumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyYumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yummyyum/YummyYumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yummyyum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyYumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
